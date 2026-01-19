8198f8aba663d11875695fb930760d53c4854002
akashpatelknit <patelakashmzp2001@gmail.com>
2026-01-20 00:01:31 +0530

    base entity created

--- src/main/java/com/springboot/ecommerce/ECommerceRestApiApplication.java
+++ src/main/java/com/springboot/ecommerce/ECommerceRestApiApplication.java
@@ -2,8 +2,10 @@ package com.springboot.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ECommerceRestApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/springboot/ecommerce/entity/BaseEntity.java
+++ src/main/java/com/springboot/ecommerce/entity/BaseEntity.java
@@ -1,4 +1,53 @@
 package com.springboot.ecommerce.entity;
 
-public class BaseEntity {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @Column(nullable = false)
+    private Instant updatedAt;
+
+    @LastModifiedBy
+    private String updatedBy;
+
+    @Column
+    private Instant deletedAt;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @PrePersist
+    protected void onCreate() {
+        Instant now = Instant.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = Instant.now();
+    }
 }
